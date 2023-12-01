b385bc180695e6f485b06f5c4d29c6e39814cf9a
xzxsxdsd <152094871+xzxsxdsd@users.noreply.github.com>
2023-12-02 01:35:07 +0300

    Update play.py

--- YukkiMusic/plugins/play/play.py
+++ YukkiMusic/plugins/play/play.py
@@ -39,7 +39,11 @@ from YukkiMusic.utils.stream.stream import stream
 # Command
 PLAY_COMMAND = get_command("PLAY_COMMAND")
 
-
+@app.on_message(
+    filters.command(["شغل","تشغيل","شغلي"],"")
+    & filters.group
+    & ~BANNED_USERS
+)
 @app.on_message(
     filters.command(PLAY_COMMAND)
     & filters.group
