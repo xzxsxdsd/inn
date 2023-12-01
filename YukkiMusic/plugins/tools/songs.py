31b9dcd8965ab1189f2a6d020775e522f72a9410
xzxsxdsd <152094871+xzxsxdsd@users.noreply.github.com>
2023-12-01 16:05:04 +0300

    Update songs.py

--- YukkiMusic/plugins/tools/songs.py
+++ YukkiMusic/plugins/tools/songs.py
@@ -9,12 +9,12 @@ from pyrogram.types import Message
 from youtubesearchpython import VideosSearch
 
 
-@app.on_message(filters.command(["song"], ["/", "!", "."]))
+@app.on_message(filters.command(["بحث","يوت","/song"],""))
 async def song(client: app, message: Message):
-    aux = await message.reply_text("🔄 𝐏𝐫𝐨𝐜𝐞𝐬𝐬𝐢𝐧𝐠 ...")
+    aux = await message.reply_text("**جاري الحث**")
     if len(message.command) < 2:
         return await aux.edit(
-            "🤖 𝐆𝐢𝐯𝐞 🙃 𝐌𝐮𝐬𝐢𝐜 💿 𝐍𝐚𝐦𝐞 😍\n💞 𝐓𝐨 🔊 𝐃𝐨𝐰𝐧𝐥𝐨𝐚𝐝 🥀 𝐒𝐨𝐧𝐠❗"
+            "**- للاسف ما اثرت على شي تأكد من كتابة اسم الفنان مع الاغنية**"
         )
     try:
         song_name = message.text.split(None, 1)[1]
@@ -34,10 +34,10 @@ async def song(client: app, message: Message):
             ],
             "outtmpl": f"downloads/{song_title}",
         }
-        await aux.edit("𝐃𝐨𝐰𝐧𝐥𝐨𝐚𝐝𝐢𝐧𝐠 ...")
+        await aux.edit("**جاري التحميـل**")
         with yt_dlp.YoutubeDL(ydl_opts) as ydl:
             ydl.download(song_link)
-        await aux.edit("𝐔𝐩𝐥𝐨𝐚𝐝𝐢𝐧𝐠 ...")
+        await aux.edit("**جاري الرفع**...")
         await message.reply_audio(f"downloads/{song_title}.mp3")
         try:
             os.remove(f"downloads/{song_title}.mp3")
@@ -45,37 +45,30 @@ async def song(client: app, message: Message):
             pass
         await aux.delete()
     except Exception as e:
-        await aux.edit(f"Error: {e}")
+        await aux.edit(f"**حدث خطأ أثناء البحث حاول مره اخرى**: {e}")
 
 
 ###### INSTAGRAM REELS DOWNLOAD
 
 
-@app.on_message(filters.command(["ig"], ["/", "!", "."]))
-async def download_instareels(c: app, m: Message):
-    try:
-        reel_ = m.command[1]
-    except IndexError:
-        await m.reply_text("Give me an link to download it...")
-        return
-    if not reel_.startswith("https://www.instagram.com/reel/"):
-        await m.reply_text("In order to obtain the requested reel, a valid link is necessary. Kindly provide me with the required link.")
-        return
-    OwO = reel_.split(".",1)
-    Reel_ = ".dd".join(OwO)
-    try:
-        await m.reply_video(Reel_)
-        return
-    except Exception:
-        try:
-            await m.reply_photo(Reel_)
-            return
-        except Exception:
-            try:
-                await m.reply_document(Reel_)
-                return
-            except Exception:
-                await m.reply_text("I am unable to reach to this reel.")
+@app.on_message(filters.command(["انستا"],""))
+async def instagram_reel(client, message):
+    if len(message.command) == 2:
+        url = message.command[1]
+        response = requests.post(f"https://lexica-api.vercel.app/download/instagram?url={url}")
+        data = response.json()
+
+        if data['code'] == 2:
+            media_urls = data['content']['mediaUrls']
+            if media_urls:
+                video_url = media_urls[0]['url']
+                await message.reply_video(f"{video_url}")
+            else:
+                await message.reply("No video found in the response. may be accountbis private.")
+        else:
+            await message.reply("Request was not successful.")
+    else:
+        await message.reply("Please provide a valid Instagram URL using the /reels command.")
 
 
 
