3bb8d536e822731683cc409efe5c1b535aa2c197
xzxsxdsd <152094871+xzxsxdsd@users.noreply.github.com>
2023-12-02 08:38:58 +0300

    Update wwm.py

--- YukkiMusic/plugins/linda/wwm.py
+++ YukkiMusic/plugins/linda/wwm.py
@@ -9,7 +9,7 @@ def get_current_time():
     current_time = datetime.now(tz)
     return current_time.strftime("%Y-%m-%d %H:%M:%S %Z%z")
 
-@app.on_message(filters.command(["Time"],""))
+@app.on_message(filters.command(["الوقت"],""))
 def send_time(client, message):
     time = get_current_time()
     client.send_message(message.chat.id, f"Current time in India: {time}")
