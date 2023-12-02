03dc432dc8eae6ec0c63c5a7ae61c0d262312950
xzxsxdsd <152094871+xzxsxdsd@users.noreply.github.com>
2023-12-02 07:47:31 +0300

    Update wwm.py

--- YukkiMusic/plugins/linda/wwm.py
+++ YukkiMusic/plugins/linda/wwm.py
@@ -1,56 +1,37 @@
-import os
-import aiohttp
-import aiofiles
-from aiohttp import ContentTypeError
-from YukkiMusic import app as app
-from pyrogram import filters
-
-def check_filename(filroid):
-    if os.path.exists(filroid):
-        no = 1
-        while True:
-            ult = "{0}_{2}{1}".format(*os.path.splitext(filroid) + (no,))
-            if os.path.exists(ult):
-                no += 1
-            else:
-                return ult
-    return filroid
-
-async def RemoveBG(input_file_name):
-    headers = {"X-API-Key": "u4x2416NAQVefYsfwbzrw7VE"}
-    files = {"image_file": open(input_file_name, "rb").read()}
-    async with aiohttp.ClientSession() as ses:
-        async with ses.post(
-            "https://api.remove.bg/v1.0/removebg", headers=headers, data=files
-        ) as y:
-            contentType = y.headers.get("content-type")
-            if "image" not in contentType:
-                return False, (await y.json())
-
-            name = check_filename("alpha.png")
-            file = await aiofiles.open(name, "wb")
-            await file.write(await y.read())
-            await file.close()
-            return True, name
+from ... import *
+from pyrogram import *
+from pyrogram.types import *
 
 
-@app.on_message(filters.command("rmbg"))
-async def rmbg(bot, message):
-  rmbg = await message.reply("Processing...") 
-  replied = message.reply_to_message
-  if not replied:
-      return await rmbg.edit("Reply to a photo to Remove it's Backgroud")
+@app.on_message(filters.command(["bin", "ccbin", "bininfo"], [".", "!", "/"]))
+async def check_ccbin(client, message):
+    if len(message.command) < 2:
+        return await message.reply_text(
+            "<b>Please Give Me a Bin To\nGet Bin Details !</b>"
+        )
+    try:
+        await message.delete()
+    except:
+        pass
+    aux = await message.reply_text("<b>Checking ...</b>")
+    bin = message.text.split(None, 1)[1]
+    if len(bin) < 6:
+        return await aux.edit("<b>❌ Wrong Bin❗...</b>")
+    try:
+        resp = await api.bininfo(bin)
+        await aux.edit(f"""
+<b>💠 Bin Full Details:</b>
 
-  if replied.photo:
-      photo = await bot.download_media(replied)
-      x, y = await RemoveBG(photo)
-      os.remove(photo)
-      if not x:
-          bruh = y["errors"][0]
-          details = bruh.get("detail", "")
-          return await rmbg.edit(f"ERROR ~ {bruh['title']},\n{details}")
-      await message.reply_photo(photo=y,caption="Here is your Image without Background")
-      await message.reply_document(document=y)
-      await rmbg.delete()
-      return os.remove(y)
-  await rmbg.edit("Reply only to a photo to Remove it's Background")
+<b>🏦 Bank:</b> <tt>{resp.bank}</tt>
+<b>💳 Bin:</b> <tt>{resp.bin}</tt>
+<b>🏡 Country:</b> <tt>{resp.country}</tt>
+<b>🇮🇳 Flag:</b> <tt>{resp.flag}</tt>
+<b>🧿 ISO:</b> <tt>{resp.iso}</tt>
+<b>⏳ Level:</b> <tt>{resp.level}</tt>
+<b>🔴 Prepaid:</b> <tt>{resp.prepaid}</tt>
+<b>🆔 Type:</b> <tt>{resp.type}</tt>
+<b>ℹ️ Vendor:</b> <tt>{resp.vendor}</tt>"""
+        )
+    except:
+        return await aux.edit(f"""
+🚫 BIN not recognized. Please enter a valid BIN.""")
