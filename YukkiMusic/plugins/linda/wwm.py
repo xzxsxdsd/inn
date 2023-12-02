4e2519cff89f7864998e3549146c7789dfcbcc0d
xzxsxdsd <152094871+xzxsxdsd@users.noreply.github.com>
2023-12-02 08:58:41 +0300

    Update wwm.py

--- YukkiMusic/plugins/linda/wwm.py
+++ YukkiMusic/plugins/linda/wwm.py
@@ -1,6 +1,6 @@
 from traceback import format_exc
 from pyrogram.types import Message, InlineKeyboardButton, InlineKeyboardMarkup
-from search_engine_parser.core.exceptions import NoResultsFound, NoResultsOrTrafficError
+
 from YukkiMusic import app
 from pyrogram import filters
 
